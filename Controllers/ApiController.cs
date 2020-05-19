6df7d52f992b5f48c8f9c5253eb387c51f319f46
Sergey Murzin <atlant-t@bk.ru>
2020-05-19 16:21:32 +0300

    Initialize Cashflow Api Server

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CashflowApi.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class ApiController : ControllerBase
+    {
+        private readonly ILogger<ApiController> _logger;
+
+        public ApiController(ILogger<ApiController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Cashflow Api";
+        }
+    }
+}
